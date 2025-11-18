21ba32badbd10def603e94611128d355f4f52bb1
César Filipe <filipegomes012642@gmail.com>
2025-11-18 19:27:33 -0300

    feat: Criando repositorio para melhor implementação em memória e http

--- app/src/main/java/br/com/fooddelivery/tialudeliveryapp/MainActivity.kt
+++ app/src/main/java/br/com/fooddelivery/tialudeliveryapp/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.fooddelivery.tialudeliveryapp.ui.screen.RegisterMenuScreen
 import br.com.fooddelivery.tialudeliveryapp.ui.theme.TiaLuDeliveryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TiaLuDeliveryAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RegisterMenuScreen(
+                    Greeting(
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/br/com/fooddelivery/tialudeliveryapp/network/KtorHttpClient.kt
+++ app/src/main/java/br/com/fooddelivery/tialudeliveryapp/network/KtorHttpClient.kt
@@ -1,20 +1,13 @@
 package br.com.fooddelivery.tialudeliveryapp.network
 
-import android.util.Log
-import br.com.fooddelivery.tialudeliveryapp.network.model.CreateMenuRequest
-import br.com.fooddelivery.tialudeliveryapp.network.model.CreateMenuResponse
 import br.com.fooddelivery.tialudeliveryapp.network.model.ProductDetailRequest
 import br.com.fooddelivery.tialudeliveryapp.network.model.ProductDetailResponse
-import io.ktor.http.ContentType
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 
 object KtorHttpClient {
@@ -32,33 +25,12 @@ object KtorHttpClient {
         return requireGet(url = "$BASE_URL/api/v1/merchant/R1001/products/${data.productId}")
     }
 
-    suspend fun postCreateMenu(data: CreateMenuRequest): Result<CreateMenuResponse> {
-        Log.d("KtorHttpClient",  "data: $data")
-        return requirePost(url = "$BASE_URL/menu/create", body = data)
-    }
-
     private suspend inline fun <reified T> requireGet(
         url: String
     ): Result<T> {
         return try {
             Result.success(
                 client.get(url).body()
-            )
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    private suspend inline fun <reified T, reified R> requirePost(
-        url: String,
-        body: R
-    ): Result<T> {
-        return try {
-            Result.success(
-                client.post(url) {
-                    contentType(ContentType.Application.Json)
-                    setBody(body)
-                }.body()
             )
         } catch (e: Exception) {
             Result.failure(e)
