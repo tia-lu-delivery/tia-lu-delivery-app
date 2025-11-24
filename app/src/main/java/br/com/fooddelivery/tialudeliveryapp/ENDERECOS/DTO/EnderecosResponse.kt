e26475aa646d2700f18376b5756daa21468ddec1
alexsander1danilo <118858236+alexsander1danilo@users.noreply.github.com>
2025-11-24 01:25:16 -0300

    Update EnderecosResponse.kt

--- app/src/main/java/br/com/fooddelivery/tialudeliveryapp/ENDERECOS/DTO/EnderecosResponse.kt
+++ app/src/main/java/br/com/fooddelivery/tialudeliveryapp/ENDERECOS/DTO/EnderecosResponse.kt
@@ -1,7 +1,5 @@
 package br.com.fooddelivery.tialudeliveryapp.ENDERECOS.DTO
 
-package com.exemplo.enderecos.dto
-
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class EnderecosResponse(
